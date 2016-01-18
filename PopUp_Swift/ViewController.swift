82ff0b7b30f9b1963aac9d4b34f9750f6ec1e0f2
Valentin Granados <valefcfhc@gmail.com>
2016-01-18 13:27:54 -0600

    Initial Commit

---
+++ PopUp_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopUp_Swift
+//
+//  Created by Emmanuel Valentín Granados López on 18/01/16.
+//  Copyright © 2016 DevWorms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
